ad9b25816a74cb7e2ab019842c0c092a0e0a11fa
Sych Oleh <sicholeg1@gmail.com>
2026-05-10 18:35:09 +0300

    minor changes

--- home_work4/homework4.4.js
+++ home_work4/homework4.4.js
@@ -1,15 +1,15 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-switch (numOrStr) {
-  case null:
+switch (true) {
+  case numOrStr === null:
     console.log('ви скасували')
     break;
-  case '':
+  case numOrStr.trim() === '':
     console.log('Empty String')
     break;
   case isNaN(+numOrStr):
-    console.log(' number is Ba_NaN')
+    console.log('number is Ba_NaN')
     break;
   default:
     console.log('OK!')
