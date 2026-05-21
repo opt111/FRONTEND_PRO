da208fe1553eca865f1332d8e01565dbaaced2d1
Sych Oleh <sicholeg1@gmail.com>
2026-05-22 00:33:04 +0300

    add homework 8.3

--- home_work8/homework8.3.js
+++ home_work8/homework8.3.js
@@ -1,12 +1,19 @@
+
+
 function myFunc() {
+  let num
+
   for (let i = 1; i <= 10; i++) {
-    let num = +prompt('Введіть число');
+    num = +prompt('Введіть число');
     if (num <= 100) {
       alert('Число меньше 100. Спробуйте ще раз')
     } else {
-      return alert(`Ви ввели: ${num}`)
+      return alert(`Останнє введене число: ${num}`)
     }
   }
-}
+  alert(`Останнє введене число: ${num}`)
 
+
+
+}
 console.log(myFunc())
