a469771d08a4655600cd7f66a4b41fe63e4eff76
Sych Oleh <sicholeg1@gmail.com>
2026-05-25 22:16:10 +0300

    add homework9

---
+++ home_work9/homework9.1.js
@@ -0,0 +1,19 @@
+let ladder = {
+  step: 0,
+  up: function () {
+    this.step += 1
+    return this
+  },
+
+  down: function () {
+    this.step -= 1
+    return this
+  },
+  
+  showStep: function () {
+    console.log(this.step)
+    return this
+  }
+};
+
+ladder.up().up().up().up().down().showStep().up().up().showStep();
